e9e01a680345a563885829c26f43d2904406d257
Kjablonska <38289305+Kjablonska@users.noreply.github.com>
2018-04-26 12:28:48 +0200

    Update board.c

--- board.c
+++ board.c
@@ -6,9 +6,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-void printBoard(int player, Board *p) {	// Prints board on the screen
-	int i, j;
 
+/***********************************************************************************
+Function prints board on the screen. 
+It takes as a parameters information about player and pointer to the Board structure.
+************************************************************************************/
+void printBoard(int player, Board *p) {
+	int i, j;
+	
+	/* Prints columns numbers from A to J. */
 	printf("    ");
 	for (char c = 'A'; c <= 'J'; ++c)
 		printf("%c      ", c);
@@ -33,6 +39,10 @@ void printBoard(int player, Board *p) {	// Prints board on the screen
 	printf("\n");
 }
 
+
+/* Function set boards for both players and fills them with 0'. 
+It takes as a paremter poninter to the structure Board.
+It returns a pointer to the structure Board. */
 Board* setBoard(Board *p) {	// Fills global boards with 0's
 	int i, j;
 
@@ -49,7 +59,9 @@ Board* setBoard(Board *p) {	// Fills global boards with 0's
 }
 
 /*******************************************************************
-Returns 1 if you can place the tile, 0 oterwise.
+Function checks if the tile can be placed in selected place.
+It takes as a parameter information about player and pointers to structure Board and Tile.
+It returns 0 if placing the tile in a selected place is unpossible, 1 otherwise. 
 ********************************************************************/
 int canplaceTile(int player, Board *p, Tiles *t) {
 	const char *empty = "00000";
@@ -94,14 +106,23 @@ int canplaceTile(int player, Board *p, Tiles *t) {
 	return 1;
 }
 
+/* Function place the tile on a board. 
+It takes as parameters pointers to structures Board and Tile, number of tile and 
+information about player.
+It returns the pointer to Board structure. */
 Board* placeTile(Tiles *t, int tile, int player, Board *p) {
+	// PLAYER 1.
 	if (player == PLAYER1) {
+		// Changing information about column from char to int.
 		p->column1 = p->column1 - 'A';
+		// Array (board) numerated form 0 so we need to substract 1 form information about row.
 		p->row1 = p->row1 - 1;
 		if (canplaceTile(player, p, t) == 1) {
+			// Placing the tile.
 			p->board_player1[p->row1][p->column1] = t->tiles[tile];
 		}
 	}
+	// PLAYER 2.
 	else {
 		p->column2 = p->column2 - 'A';
 		p->row2 = p->row2 - 1;
