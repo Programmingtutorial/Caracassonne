9617c5514a421be347892d19c88824fdada43f73
Kjablonska <38289305+Kjablonska@users.noreply.github.com>
2018-05-15 23:21:39 +0200

    Update interactive.h

--- interactive.h
+++ interactive.h
@@ -1,8 +1,8 @@
 #ifndef INTERACTIVE_H
 #define INTERACTIVE_H
 
-#include "C:\Users\kjabl\OneDrive\Pulpit\EiTI\Carcassonne\Tiles\Project1\tile.h"
-#include "C:\Users\kjabl\OneDrive\Pulpit\EiTI\Carcassonne\Board\Board\board.h"
+#include "tile.h"
+#include "board.h"
 
 #include <stdio.h>
 #include <stdlib.h>
