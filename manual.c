0651fe796a2b190750eca5bf57f68c35215bf495
jwojcieszuk <38286342+jwojcieszuk@users.noreply.github.com>
2018-05-23 20:41:28 +0200

    Add files via upload

---
+++ manual.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void printManual()
+{
+	printf("You can choose rows from 1 to 10.\n");
+	printf("You can choose columns from A to J. Use only capital letters!\n");
+	printf("There are 14 tiles that can be used in this game.\n");
+	printf("Tile can be rotated to the left, right, and down, by typing L, R or D.\n");
+	printf("To end game, press ctrl+C.\n");
+}
