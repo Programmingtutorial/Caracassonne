77f6dc11de108037f9a2b828b9b07debe8696181
Kjablonska <38289305+Kjablonska@users.noreply.github.com>
2018-06-03 18:02:52 +0200

    Update interactive.h

--- interactive.h
+++ interactive.h
@@ -1,8 +1,8 @@
 #ifndef INTERACTIVE_H
 #define INTERACTIVE_H
 
-#include "C:\Users\kjabl\OneDrive\Pulpit\EiTI\Carcassonne\Tiles\Project1\tile.h"
-#include "C:\Users\kjabl\OneDrive\Pulpit\EiTI\Carcassonne\Board\Board\board.h"
+#include "tile.h"
+#include "board.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,6 +31,6 @@ Board* selectPlace(Board *p);
 /**
 *
 */
-Tiles* makeMove(Board *p, Tiles *t, FILE *board_player, FILE *tile);
+Tiles* makeMove(Board *p, Tiles *t);
 
 #endif
