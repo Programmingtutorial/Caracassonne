0ed3d662234fc112194812493ba24c4c36a4e184
Kjablonska <38289305+Kjablonska@users.noreply.github.com>
2018-06-06 23:34:20 +0200

    Update interactive.h

--- interactive.h
+++ interactive.h
@@ -13,23 +13,27 @@
 */
 int selectMode();
 
+
 /**
-*
+* Function asks a user to select tile.
 */
 int selectTile(Tiles *t);
 
+
 /**
-*
+* Function ask user to select roatation.
 */
 Tiles* selectRotation(Tiles *t);
 
+
 /**
-*
+* Function ask user to select the place on a board where to put selected tile.
 */
 Board* selectPlace(Board *p);
 
+
 /**
-*
+* Function makes move for interactive mode.
 */
 Tiles* makeMove(Board *p, Tiles *t);
 
