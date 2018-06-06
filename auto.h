a154ae1259332a166d86b338cde37c987c4daf1d
Kjablonska <38289305+Kjablonska@users.noreply.github.com>
2018-06-06 23:30:15 +0200

    Update auto.h

--- auto.h
+++ auto.h
@@ -9,47 +9,59 @@
 #include <stdio.h>
 
 /**
-*
+* Function reads board form a file and set board array.
 */
 Board* initializeBoard(FILE *board_auto, Board *p);
 
+
 /**
-*
+* Function writes board array to the file.
 */
 void fillBoardFile(FILE *board, Board *p);
 
+
 /**
-*
+* Function writes tile array to the file.
 */
 void fillTileFile(FILE *tile, Tiles *t);
 
+
 /**
-*
+* Function makes the first move.
+It places randomly selected tile on down right corner of the board.
 */
 Board* firstMove(int tile, Tiles *t, Board *p);
 
+
 /**
-*
+* Function randomly select tile.
 */
 int randomFirstTile(void);
 
+
 /**
-*
+* Function checks if there is any tile on a board already.
+If not, places a tile in a middle of a board as a first move- fistMove() function.
+Otherwise it takes first free place from a right. 
+If there is already any tile on a board function sets infromation about row and column where this tile is.
 */
 Board* checkBoard(Board *p);
 
+
 /**
-*
+* Function takes the next tile in realtion to previously checked.
 */
 int takeTile(int number, Tiles *t);
 
+
 /**
-*
+* Function checks if the tile can be placed.
 */
 int canPlaceTileAUTO(int number, Board *p, Tiles *t);
 
+
 /**
-*
+* Function makes the move automatically.
 */
 Tiles* makeMoveAUTO(Board *p, Tiles *t, FILE *board_auto, FILE *tile);
 
